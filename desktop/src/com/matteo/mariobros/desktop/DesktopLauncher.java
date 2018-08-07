0276fc71c4a4840e27dbfce01766c71821b9fe6a
Ash6390 <matteo.colombi259@gmail.com>
2018-08-07 14:48:18 +0200

    Add files via upload

---
+++ desktop/src/com/matteo/mariobros/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.matteo.mariobros.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.matteo.mariobros.MarioBros;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MarioBros(), config);
+	}
+}
